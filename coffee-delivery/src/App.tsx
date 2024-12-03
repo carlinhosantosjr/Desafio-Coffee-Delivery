51a5c02873c0426341f4d416d36f3102d1019c4b
carlinhosantosjr <carlinhosantosjr@hotmail.com>
2024-12-03 11:34:04 -0300

    assets | defaultTheme |

--- coffee-delivery/src/App.tsx
+++ coffee-delivery/src/App.tsx
@@ -1,9 +1,16 @@
+import { GlobalStyle } from './styles/global'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import irlandes from './assets/logo.svg'
+
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <div>
         <h1>hahahaha</h1>
+        <img src={irlandes} alt="" />
       </div>
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
